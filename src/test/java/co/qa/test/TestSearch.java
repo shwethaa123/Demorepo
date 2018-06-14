3714a3a2d2fdeda58f23f4460a326f629d036984
shwethaa123 <shwethaa123@yahoo.com>
2018-06-14 19:21:12 +0530

    added test search

---
+++ src/test/java/co/qa/test/TestSearch.java
@@ -0,0 +1,15 @@
+package co.qa.test;
+
+import org.testng.annotations.Test;
+
+public class TestSearch {
+	public class testSearch {
+		
+		@Test
+		public void test2  ()
+		{
+			System.out.println("Test 2");
+		}
+	}
+
+}
